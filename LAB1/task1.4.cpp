eb212d3d913dd1f85f8d7c194e36a8415c1cc06d
Morgan <morno368@student.liu.se>
2024-10-09 12:15:15 +0200

    first draft of data structures

--- LAB1/task1.4.cpp
+++ LAB1/task1.4.cpp
@@ -9,10 +9,19 @@
 // 
 // Warning: You are expected to produce a lot of output data for this problem. Make sure that you don't flush the output buffer after each line, since this might make your otherwise correct solution too slow. 
 
+#include <vector>
 
-// Each node in a disjoint-set forest consists of a pointer and some auxiliary information, either a size or a rank (but not both). The pointers are used to make parent pointer trees, where each node that is not the root of a tree points to its parent. To distinguish root nodes from others, their parent pointers have invalid values, such as a circular reference to the node or a sentinel value. Each tree represents a set stored in the forest, with the members of the set being the nodes in the tree. Root nodes provide set representatives: Two nodes are in the same set if and only if the roots of the trees containing the nodes are equal.
-// 
-// Nodes in the forest can be stored in any way convenient to the application, but a common technique is to store them in an array. In this case, parents can be indicated by their array index. Every array entry requires Θ(log n) bits of storage for the parent pointer. A comparable or lesser amount of storage is required for the rest of the entry, so the number of bits required to store the forest is Θ(n log n). If an implementation uses fixed size nodes (thereby limiting the maximum size of the forest that can be stored), then the necessary storage is linear in n. 
+struct Node {
+	Node* parent; //NULL if root
+	int size;
+	
+	Node(Node* parent, int size) : parent{parent}, size{size} {};
+};
+
+struct DisjointSet {
+	// I THINK this should suffice
+	std::vector<Node> trees;
+};
 
 // Making new sets
 // 
