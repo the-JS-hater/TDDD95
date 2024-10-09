3c7cb442cab9ee8f669a56bf4eed9a93888f057a
Morgan <morno368@student.liu.se>
2024-10-09 12:17:28 +0200

    adding empty files for the rest of the assignments in lab 1

---
+++ LAB1/task1.5.cpp
@@ -0,0 +1,8 @@
+//  Implement a data structure to handle prefix sums of an array a[0], a[1], ..., a[N-1], with the following operations:
+// 
+//     add(index i, number delta) - increase a[i] with delta
+//     number sum(index end) - compute the sum of the first numbers up to, but not including, a[end]
+// 
+// Recommended time complexity: O(log n) for both operations, where n is the number of elements in the array.
+// 
+// Warning: Even with a correct imlementation, Python is probably too slow to solve this assignment. 
