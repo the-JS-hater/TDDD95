fc468ff32790143b9a5ef5edccf88287d3c817a5
Morgan <mnordberg01@gmail.com>
2025-02-05 22:35:59 +0100

    solved help!

--- UPG1/set1/help.py
+++ UPG1/set1/help.py
@@ -1,58 +1,37 @@
-from itertools import pairwise
+is_pattern = lambda w : w[0] == '<'
+replace = lambda l, o, n : [n if x == o else x for x in l]
+
+f = open(0).read().splitlines()[1:]
+o = []
+for i in range(0, len(f), 2):
+    a, b = f[i].split(" "), f[i+1].split(" ")
+    if len(a) != len(b):
+        o.append("-")
+        continue
+    flag = True
+    while flag:
+        flag = False
+        p = zip(a, b)
+        for l, r in p:
+            if is_pattern(l) and not is_pattern(r):
+                a = replace(a, l, r)
+                flag = True
+                continue
+            if is_pattern(r) and not is_pattern(l):
+                b = replace(b, r, l)
+                flag = True
+                continue
+    p = zip(a, b)
+    for l, r in p:
+        if is_pattern(l) and is_pattern(r):
+            a = replace(a, l, 'a')                
+            b = replace(b, r, 'a')
+            flag = True
+            continue
+    if a != b:
+        o.append("-")
+    else: 
+        o.append(a)
 
 
-def main():
-    f = [line.replace("\n", "").split(' ') for line in open(0, 'r').readlines()[1:]]
-    [print(res) for res in [helper(f[i], f[i+1], {}, {}) for i in range(0, len(f) - 1, 2)]]
-
-
-def helper(l1, l2, mem_l, mem_r):
-    if not l1 and not l2:
-        return ''
-    if (not l1 and l2) or (l1 and not l2):
-        return '-'
-    left, right = l1[0], l2[0]
-    l_rest, r_rest = l1[1:], l2[1:]
-    
-    rest = helper(l_rest, r_rest, mem_l, mem_r)
-    if rest == '-':
-        return rest
-
-    if left[0] == '<' and right[0] == '<':
-        if left in mem_l and right in mem_r:
-            if mem_l[left] != mem_r[right]:
-                return '-'
-            else:
-                return mem_l[left] + " " + rest
-            
-        if right in mem_r:
-            return mem_r[right] + " " + rest
-        if left in mem_l:
-            return mem_l[left] + " " + rest
-
-        mem_l[left] = 'a'
-        return 'a' + " " + rest
-
-    if left[0] == '<':
-        if left in mem_l and mem_l[left] != right:
-            return '-'
-        mem_l[left] = right
-        return right + " " + rest
-    
-    if right[0] == '<':
-        if right in mem_r and mem_r[right] != left:
-            return '-'
-        mem_r[right] = left
-        return left + " " + rest
-    
-    if left == right:
-        return left + " " + rest
-    else:
-        return '-'
-    
-
-if __name__ == "__main__":
-    main()
-
-    
-
+print("\n".join([" ".join(r) for r in o]))
